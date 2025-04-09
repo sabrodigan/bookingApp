289fd7dce68f6d710a382df55730c28d754a3c9e
sabrodigan <sabrodigan@gmail.com>
2025-04-09 15:15:51 +0000

    resolved errors in make-reservation page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
-		log.Fatal("cannot create template cache")
+		log.Fatal("cannot create template cache: ", err)
 	}
 
 	app.TemplateCache = tc
